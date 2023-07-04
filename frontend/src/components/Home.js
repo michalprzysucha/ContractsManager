6708dc6998e8a2ebcea6cf88828555943e101ffe
michalprzysucha <mpbeliar@gmail.com>
2023-07-04 16:16:12 +0200

    added css for summaries on home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,6 +5,11 @@ function Home(){
         <>
             <h1>Witamy na stronie do zarządzania przetargami</h1>
             <DateAndTime />
+            <div id="summaries_wrapper">
+                <div class="summary_box"><h3>Aktywne przetargi z największą liczbą ofert</h3></div>
+                <div class="summary_box"><h3>Przetargi bliskie zakończenia</h3></div>
+                <div class="summary_box"><h3>Przetargi o największym budżecie</h3></div>
+            </div>
         </>
     );
 }
