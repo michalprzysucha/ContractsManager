1d71b47e6ba08b3b28088b704cc05d4dfb1bf1f8
Hono Z <honorata.z95@gmail.com>
2023-07-10 18:26:46 +0200

    cleaned offerForm

--- frontend/src/components/OfferForm.js
+++ frontend/src/components/OfferForm.js
@@ -1,6 +1,5 @@
 import {useState, useEffect} from "react";
 import {Navigate} from "react-router-dom";
-import {useParams} from "react-router-dom";
 
 const OfferForm = (props) => {
     const [error, setError] = useState(null);
